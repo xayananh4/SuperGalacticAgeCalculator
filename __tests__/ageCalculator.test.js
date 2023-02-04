9f9a25551e366ce385f9bf5b76f977fd7d6a9fcb
xayananh4 <xayananh4@gmail.com>
2023-02-03 18:12:45 -0800

    test: should return the user age in years. Function getUserAgeInYears recieved good pass

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -1,8 +1,8 @@
-import{Age}  from './../src/age.js';
+import { Age } from './../src/age.js';
 
 describe('Age', () => {
 
-    test('should return the user date of birth', () => {
+  test('should return the user date of birth', () => {
     const userDob = new Age('July 14, 1984 01:01:01');
     expect(userDob.userDateOfBirth).toEqual('July 14, 1984 01:01:01');
   });
--- src/age.js
+++ src/age.js
@@ -4,11 +4,18 @@ export class Age {
     this.userDateOfBirth = dateOfBirth;
   }
 
-  getUserAgeInYears(dateOfBirthString)
-  {
+  getUserAgeInYears(dateOfBirthString) {
+    let today = new Date();
 
+    let birthDate = new Date(this.userDateOfBirth);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    let m = today.getMonth() - birthDate.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
   }
-
-
-
 }
