0c90912b4c3579bb56cc6ad8dec3a72f593a24fc
xayananh4 <xayananh4@gmail.com>
2023-02-03 17:57:34 -0800

    refactor construtor to take only one paramter, dateOfBirth. For test 'should return the user date of birth'. Test is still passing

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -3,10 +3,8 @@ import{Age}  from './../src/age.js';
 describe('Age', () => {
 
     test('should return the user date of birth', () => {
-    const userAge = new Age('July 14,', '1984', '01:01:01');
-    expect(userAge.MonthAndDate).toEqual('July 14,');
-    expect(userAge.Year).toEqual('1984');
-    expect(userAge.TimeOfBirth).toEqual('01:01:01');
+    const userAge = new Age('July 14, 1984 01:01:01');
+    expect(userAge.userDateOfBirth).toEqual('July 14, 1984 01:01:01');
   });
 
   test('should return age in years for the user', () => {
--- src/age.js
+++ src/age.js
@@ -1,9 +1,13 @@
 export class Age {
 
-  constructor(MonthAndDate,Year,TimeOfBirth) {
-    this.MonthAndDate = MonthAndDate;
-    this.Year = Year;
-    this.TimeOfBirth = TimeOfBirth; 
+  // constructor(MonthAndDate,Year,TimeOfBirth) {
+  //   this.MonthAndDate = MonthAndDate;
+  //   this.Year = Year;
+  //   this.TimeOfBirth = TimeOfBirth; 
+  // }
+
+    constructor(dateOfBirth) {
+    this.userDateOfBirth = dateOfBirth;
   }
 
 
