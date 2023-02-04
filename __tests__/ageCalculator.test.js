bcaa59efb47f86d557ebab088919d10404817863
xayananh4 <xayananh4@gmail.com>
2023-02-03 22:26:33 -0800

    test: should return the user age in days. Verified good fail, test convertAgeToDays constructor

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -8,9 +8,16 @@ describe('Age', () => {
   });
 
   test('should return the user age in years', () => {
-    const userDob = new Age('July 14, 1984 01:01:01');
-    expect(userDob.getUserAgeInYears(userDob)).toEqual(38);
+    const userDob = new Age('July 02, 1985 01:01:01');
+    expect(userDob.getUserAgeInYears(userDob)).toEqual(37);
 
   });
+
+  test('should return the user age in days', () => {
+    const userDob = new Age('July 02, 1985 01:01:01');
+    expect(userDob.convertAgeToDays(userDob)).toEqual(1);
+
+  });
+
 
 });
--- src/age.js
+++ src/age.js
@@ -4,18 +4,18 @@ export class Age {
     this.userDateOfBirth = dateOfBirth;
   }
 
-  getUserAgeInYears(dateOfBirthString) {
+  getUserAgeInYears(userDateOfBirth) {
     let today = new Date();
-
     let birthDate = new Date(this.userDateOfBirth);
-
     let age = today.getFullYear() - birthDate.getFullYear();
-
     let m = today.getMonth() - birthDate.getMonth();
-
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
       age--;
     }
     return age;
   }
+
+  convertAgeToDays(age) {
+ 
+    }
 }
