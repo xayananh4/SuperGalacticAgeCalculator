c8acc798f2d3e0dccdb093503a1de6ea9ca4eff9
xayananh4 <xayananh4@gmail.com>
2023-02-05 14:55:05 -0800

    test: 'should show the years that have pass since user past birthday'.  Receiving Good Pass

--- src/age.js
+++ src/age.js
@@ -45,12 +45,12 @@ export class Age {
     return new Array("earth", "mercury", "venus", "mars", "jupiter");
   }
 
-  calculatorYearsSinceLastBirthday(userAge,pastBirthday) {
-    // let diff = Math.abs(userAge - pastBirthday);
-    // let day = 1000 * 60 * 60 * 24;
-    // let days = Math.floor(diff / day);
-    // let months = Math.floor(days / 31);
-    // let years = Math.ceil(months / 12);
-    // years;
+  calculatorYearsSinceLastBirthday(userAge, pastBirthday) {
+    let diff = Math.abs(userAge - pastBirthday);
+    let day = 1000 * 60 * 60 * 24;
+    let days = Math.floor(diff / day);
+    let months = Math.floor(days / 31);
+    let years = Math.ceil(months / 12);
+    return years;
   }
 }
